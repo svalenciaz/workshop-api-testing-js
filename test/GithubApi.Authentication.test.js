5fb8c83ff91a261e84ad29ee241136999eb65c67
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-11-10 16:59:44 -0500

    consuming GET methods

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -11,7 +11,7 @@ describe('Github Api Test', () => {
     it('Via OAuth2 Tokens by Header', async () => {
       const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
+        .set('User-Agent', 'request');
 
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
